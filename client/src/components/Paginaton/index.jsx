981f44706b9c8dd1c712d77e83a7a5f26f0e5171
James Okolie <zebedee788@gmail.com>
2023-07-25 22:58:12 +0300

    front end 70%

---
+++ client/src/components/Paginaton/index.jsx
@@ -0,0 +1,28 @@
+import styles from "./styles.module.css";
+
+const Pagination = ({ page, total, limit, setPage }) => {
+    const totalPages = Math.ceil(total / limit);
+
+    const onclick = (newPage) => {
+        setPage(newPage + 1);
+    }
+
+    return (
+        <div className={styles.container}>
+            {totalPages > 0 && [...Array(totalPages)].map((val, index) => {
+                <button className={
+                    page === index + 1
+                        ? `${styles.page_btn} ${styles.active}`
+                        : styles.page_btn
+                }
+                    key={index}
+                    onClick={() => onclick(index)}
+                >
+                    {index + 1}
+                </button>
+            })}
+        </div>
+    )
+}
+
+export default Pagination;
